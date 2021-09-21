f9959ac6fcbc2bd24504e31a342de8e88149b2f9
Akhdan Faiz <akhdan.faiz28899@gmail.com>
2021-09-21 21:34:34 +0700

    add middleware - verifyJWT

---
+++ middleware/auth/index.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const fs = require("fs");
+const { verify } = require("jsonwebtoken");
+
+module.exports={
+ verifyJWT: (req, res, next)=>{
+    let token = req.headers['x-access-token']
+    const payload = verify(token, fs.readFileSync(path.join(__dirname,'..','..','jwt.public')),{ algorithms: ['RS256'] })
+    req.body.userId = payload.aud
+    next()
+ }
+}
