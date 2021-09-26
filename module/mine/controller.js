c7fe08e9d89d3ae49341e5310f1c629bdc750774
Akhdan Faiz <akhdan.faiz28899@gmail.com>
2021-09-26 21:07:26 +0700

    install mysql for knex dan uid

--- module/mine/controller.js
+++ module/mine/controller.js
@@ -1,4 +1,5 @@
 const Response = require('../../lib/response')
+const model = require('../../model/blockModel')
 const response = new Response()
 
 module.exports = {
@@ -11,6 +12,17 @@ module.exports = {
         if(! req.app.locals.queue.length) res.json(response.create('Tidak ada block!','Silakan tambahkan block di {POST} /block'))
         req.app.locals.queue.forEach(block => {
             req.app.locals.chain.mine(block)
+            model.saveBlock(
+                {
+                    blockNumber: block.blockNumber,
+                    nonce: block.nonce,
+                    data: JSON.stringify(block.data),
+                    hash:block.hash,
+                    prevHash: block.prevHash,
+                    difficulty: block.difficulty,
+                    createdAt: block.createdAt
+                }
+            )
         });
         req.app.locals.queue = []
         const blocks = req.app.locals.chain.getBlocks()
